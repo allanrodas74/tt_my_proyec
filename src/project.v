a9c4b593026870f3f622021db5ad608613557f73
allanrodas74 <80084449+allanrodas74@users.noreply.github.com>
2025-05-31 01:41:32 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -10,8 +10,8 @@ module tt_um_allanrodas74 (
     input  [7:0]  uio_in,
     output [7:0]  uio_out,
     output [7:0]  uio_oe,
-    input         clk,
-    input         rst_n
+    input clk,
+    input  rst_n
 );
 
     wire [7:0] result;
